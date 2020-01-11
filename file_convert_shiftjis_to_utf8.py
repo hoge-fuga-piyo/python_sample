86c1c2e8b35683519629e9879cde7a70e374cd4e
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2020-01-11 21:56:31 +0900

    バグ修正

--- file_convert_shiftjis_to_utf8.py
+++ file_convert_shiftjis_to_utf8.py
@@ -6,7 +6,7 @@ dst_file_path = "./result/utf8.txt"
 
 dst_file = open(dst_file_path, 'a')
 
-with open(src_file_path, 'r', encoding="shift_jis") as f:
+with open(src_file_path, 'r', encoding="shift_jis", newline='') as f:
   for line in f:
     print(line, end='')
     dst_file.write(line)
